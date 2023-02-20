eb5438f45abaf79b1e20bcf2cc87aef6b2ce46dd
JoshMorrison99 <josh.morrison1999@gmail.com>
2023-02-19 20:56:07 -0500

    Update isxss.py

--- isxss.py
+++ isxss.py
@@ -2,17 +2,9 @@ import requests
 import argparse
 import concurrent.futures
 import sys
-from urllib.parse import urlparse, parse_qs, urlencode
 import urllib3
+from urllib.parse import urlparse, parse_qs, urlencode
 import re
-
-class Colors:
-    RED = "\033[0;31m"
-    GREEN = "\033[0;32m"
-    LIGHT_WHITE = "\033[1;37m"
-    LIGHT_GRAY = "\033[0;37m"
-    reset = '\033[0m'
-    darkgrey = '\033[90m'
 
 def requestor(line):
     urlString = line.strip('\n')
@@ -24,63 +16,36 @@ def requestor(line):
         else:
             for i in params:
                 params[i] = 'xx1\'xx2\"xx3>xx4<'
-            query = urlencode(params)
-            reflected = []
-            header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-            response = requests.get(url.scheme + '://' + url.netloc + '/' + url.path + '?' + query, verify=False, timeout=20, headers=header)
-            page = response.text
-            if(re.search('xx1\'', page)):
-                reflected.append('\'')
-            if(re.search('xx2\"', page)):
-                reflected.append('\"')
-            if(re.search('xx3>', page)):
-                reflected.append('>')
-            if(re.search('xx4<', page)):
-                reflected.append('<')
-            if(reflected):
-                print('[%s]' % ''.join(map(str,reflected)) + " reflected in " + url.scheme + '://' + url.netloc + '/' + url.path + '?' + url.query)
+                old = params[i]
+                params[i] = 'XSS'
+                query = urlencode(params)
+                reflected = []
+                header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+                response = requests.get(url.scheme + '://' + url.netloc + '/' + url.path + '?' + query, verify=False, timeout=20, headers=header)
+                page = response.text
+                if(re.search('xx1\'', page)):
+                    reflected.append('\'')
+                if(re.search('xx2\"', page)):
+                    reflected.append('\"')
+                if(re.search('xx3>', page)):
+                    reflected.append('>')
+                if(re.search('xx4<', page)):
+                    reflected.append('<')
+                if(reflected):
+                    print('[%s]' % ''.join(map(str,reflected)) + " reflected in " + url.scheme + '://' + url.netloc + '/' + url.path + '?' + query)
+                
+                params[i] = old
 
 
-def banner():
-    print(Colors.RED + """
-     ▄█     ▄████████ ▀████    ▐████▀    ▄████████    ▄████████
-    ███    ███    ███   ███▌   ████▀    ███    ███   ███    ███
-    ███▌   ███    █▀     ███  ▐███      ███    █▀    ███    █▀
-    ███▌   ███           ▀███▄███▀      ███          ███
-    ███▌ ▀███████████    ████▀██▄     ▀███████████ ▀███████████
-    ███           ███   ▐███  ▀███             ███          ███
-    ███     ▄█    ███  ▄███     ███▄     ▄█    ███    ▄█    ███
-    █▀    ▄████████▀  ████       ███▄  ▄████████▀   ▄████████▀
-    Version 0.1                             by  @Shelledlizard4
-    """ + Colors.reset)
 
 def main():
-    banner()
     parser = argparse.ArgumentParser(description="Check for reflected XSS characters",
-    usage="cat allUrls.txt | python3 isxss.py [options]")
+    usage="cat urls.txt | python3 isxss.py")
 
-    parser.add_argument('-t', metavar='-threads', help="Specify the number of threads (default=50)")
-    parser.add_argument('-l', metavar='-list', help="Specify a file containing all the urls to be scanned")
-    parser.add_argument('-v', help="Debug mode", action='store_true')
-    global debug
-
-    args = parser.parse_args()
-
-
-    if(args.v == True):
-        debug = True
-
-    num_threads = 50
-    if(args.t != None):
-        num_threads = args.t
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-    if(sys.stdin == None):
-        with concurrent.futures.ThreadPoolExecutor(max_workers=int(num_threads)) as executor:
-            executor.map(requestor, args.l)
-    else:
-        with concurrent.futures.ThreadPoolExecutor(max_workers=int(num_threads)) as executor:
-            executor.map(requestor, sys.stdin)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=int(25)) as executor:
+        executor.map(requestor, sys.stdin)
 
 
 main()
